0714c01057587d8282c4e6445b2f29c9ff292bb2
ykn <ykn1002@163.com>
2024-09-30 00:03:54 +0800

    123213

--- locales/zh/l10n-accessControl-platformRoles-list.js
+++ locales/zh/l10n-accessControl-platformRoles-list.js
@@ -20,9 +20,9 @@ module.exports = {
   PLATFORM_ROLE_PL: '平台角色',
   // List
   PLATFORM_ROLE_EMPTY_DESC: '请创建一个平台角色。',
-  ROLE_USERS_MANAGER: '管理 KubeSphere 平台上的所有用户。',
-  ROLE_WORKSPACES_MANAGER: '管理 KubeSphere 平台上的所有企业空间。',
-  ROLE_PLATFORM_ADMIN: '管理 KubeSphere 平台上的所有资源。',
+  ROLE_USERS_MANAGER: '管理平台上的所有用户。',
+  ROLE_WORKSPACES_MANAGER: '管理平台上的所有企业空间。',
+  ROLE_PLATFORM_ADMIN: '管理平台上的所有资源。',
   ROLE_PLATFORM_REGULAR: '被邀请加入企业空间之前无法访问任何资源。',
   ROLE_PLATFORM_SELF_PROVISIONER: '创建企业空间并成为所创建的企业空间的管理员。',
   CREATION_TIME_TCAP: '创建时间',
@@ -59,7 +59,7 @@ module.exports = {
   // List > Create > Edit Permissions > Platform Settings
   PERMIGROUP_PLATFORM_SETTINGS: '平台设置',
   PERMISSION_PLATFORM_SETTINGS_MANAGEMENT: '平台设置管理',
-  PERMISSION_PLATFORM_SETTINGS_MANAGEMENT_DESC: '查看和编辑 KubeSphere平台的设置。',
+  PERMISSION_PLATFORM_SETTINGS_MANAGEMENT_DESC: '查看和编辑平台的设置。',
   // List > Edit Information
   // List > Edit Permissions
   // List > Delete
